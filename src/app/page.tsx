47afa15593406b11913ebac1a90a25039d931d5b
theo.clapperton <theoclapperton@outlook.com>
2025-06-04 18:27:08 +0100

    DEV-1 - Added jest and wrote placeholder test for the first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
