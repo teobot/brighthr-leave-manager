24a0061c14699cdcb1aa256c1e4302c57b2e701b
theo.clapperton <theoclapperton@outlook.com>
2025-06-05 20:50:13 +0100

    DEV-12 - Introduced AbsenceType enum to standardize absence types in the Absence type definition

--- src/common/types/kata.types.ts
+++ src/common/types/kata.types.ts
@@ -1,14 +1,18 @@
 // to check the type you can view the follow https://github.com/brighthr/Front-End-Tech-Tasks/blob/main/src/handlers/absences/index.js
+
+export enum AbsenceType {
+  FAMILY = "FAMILY",
+  COMPASSIONATE_LEAVE = "COMPASSIONATE_LEAVE",
+  MEDICAL = "MEDICAL",
+  SICKNESS = "SICKNESS",
+  ANNUAL_LEAVE = "ANNUAL_LEAVE",
+}
+
 export type Absence = {
   id: number;
   startDate: string;
   days: number;
-  absenceType:
-    | "FAMILY"
-    | "COMPASSIONATE_LEAVE"
-    | "MEDICAL"
-    | "SICKNESS"
-    | "ANNUAL_LEAVE";
+  absenceType: AbsenceType;
   employee: {
     firstName: string;
     lastName: string;
